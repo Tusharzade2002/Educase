318fed9983e399b42006f7e3abfd2dc82a4b702b
Tusharzade <tusharzade29@gmail.com>
2025-04-26 10:32:32 +0530

    Created home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+function Home() {
+  return (
+    <div className="flex items-center justify-center p-0  ">
+      <div className=" bg-white h-[90%] w-[300px] my-4 md:my-6 m-6 p-3 md:py-4 md:px-4 shadow-lg">
+        <div className="h-72 ">
+        
+       </div>
+        <h1 className="text-start text-2xl font-bold">Welcome to PopX</h1>
+        <p className="text-start text-sm my-1 me-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <div>
+            <Link to="/createaccount">
+          <button className="w-full bg-purple-800 text-white text-lg font-bold my-1 py-1 rounded-lg">
+            Create Account
+          </button>
+          </Link>
+          <Link to="/signin">
+          <button className="w-full bg-purple-300 font-bold my-1 py-1 rounded-lg">
+            Already Registered ? Login
+          </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
