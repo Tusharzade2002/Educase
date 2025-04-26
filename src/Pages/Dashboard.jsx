4218b139cb0e230783c52f820a6b37ec72540ff3
Tusharzade <tusharzade29@gmail.com>
2025-04-26 11:09:03 +0530

    design update

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="flex items-center justify-center p-0  ">
+      <div className=" bg-white h-[90%] w-[300px] my-4 md:my-6 m-6 p-3 md:py-4 md:px-4 shadow-lg">
+      
+        
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 function Home() {
   return (
     <div className="flex items-center justify-center p-0  ">
-      <div className=" bg-white h-[90%] w-[300px] my-4 md:my-6 m-6 p-3 md:py-4 md:px-4 shadow-lg">
+      <div className=" bg-white h-[100%]  w-[300px] my-4 md:my-6 m-6 p-3 md:py-4 md:px-4 shadow-lg">
         <div className="h-72 ">
         
        </div>
