fac6fe555566f28e215a2707e3058b9b6f788ae8
Tusharzade <tusharzade29@gmail.com>
2025-04-26 14:08:45 +0530

    change height

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="flex items-center justify-center p-0  ">
       <div className=" bg-white h-[100%]  w-[300px] my-4 md:my-6 m-6 p-3 md:py-4 md:px-4 shadow-lg">
-        <div style={{height:"300px"}}>
+        <div className='h-[500px] md:[300px]'>
            
        </div>
         <h1 className="text-start text-2xl font-bold">Welcome to PopX</h1>
